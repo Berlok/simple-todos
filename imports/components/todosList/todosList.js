e8baee2d30587ee4bd8f541b112f40364f7a6c9e
Longfei Fu <Longfei.Fu@ictu.nl>
2017-06-09 16:25:23 +0200

    added form and input

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -3,19 +3,35 @@ import angularMeteor from 'angular-meteor';
 import template from './todosList.html';
 import { Tasks } from '../../api/tasks.js';
 
- 
+
 class TodosListCtrl {
   constructor($scope) {
     $scope.viewModel(this);
- 
+
     this.helpers({
       tasks() {
-        return Tasks.find({});
+        // Show newest tasks at the top
+        return Tasks.find({}, {
+          sort: {
+            createdAt: -1
+          }
+        });
       }
     })
   }
+
+  addTask(newTask) {
+    // Insert a task into the collection
+    Tasks.insert({
+      text: newTask,
+      createdAt: new Date
+    });
+
+    // Clear form
+    this.newTask = '';
+  }
 }
- 
+
 export default angular.module('todosList', [
   angularMeteor
 ])
